5ebd081f290a8378fda8afb84d8f48b02392ef6d
PedroMachadoGRP <pedrolimass10@gmail.com>
2026-07-21 21:03:10 -0300

    refactor:adicionado getters e setter para os modelos de entidades

--- src/domain/models/admin.ts
+++ src/domain/models/admin.ts
@@ -1,9 +1,8 @@
-
 export interface IAdmin {
     id_admin: number;
     password_admin: string;
     email_admin: string;
-    role: string
+    role: string;
 }
 
 export class Admin implements IAdmin {
@@ -11,4 +10,48 @@ export class Admin implements IAdmin {
     password_admin: string;
     email_admin: string;
     role: string;
+
+    constructor(
+        id_admin: number,
+        password_admin: string,
+        email_admin: string,
+        role: string
+    ) {
+        this.id_admin = id_admin;
+        this.password_admin = password_admin;
+        this.email_admin = email_admin;
+        this.role = role;
+    }
+
+    getId_admin(): number {
+        return this.id_admin;
+    }
+
+    setId_admin(id_admin: number): void {
+        this.id_admin = id_admin;
+    }
+
+    getPassword_admin(): string {
+        return this.password_admin;
+    }
+
+    setPassword_admin(password_admin: string): void {
+        this.password_admin = password_admin;
+    }
+
+    getEmail_admin(): string {
+        return this.email_admin;
+    }
+
+    setEmail_admin(email_admin: string): void {
+        this.email_admin = email_admin;
+    }
+
+    getRole(): string {
+        return this.role;
+    }
+
+    setRole(role: string): void {
+        this.role = role;
+    }
 }
--- src/domain/models/delivery.ts
+++ src/domain/models/delivery.ts
@@ -1,16 +1,14 @@
 import { Material } from "./material";
-import { User } from "./User";
-
+import { User } from "./user";
 
 export interface IDelivery {
     id_delivery: number;
     delivery_Local: string;
     material_type: string;
-    status: status
+    status: status;
     material_quantity: number;
     fk_user: User;
-    fk_material:Material;
-
+    fk_material: Material;
 }
 
 export enum status {
@@ -19,7 +17,7 @@ export enum status {
     CANCELED = "CANCELED",
 }
 
-export class Delivery implements IDelivery{
+export class Delivery implements IDelivery {
     id_delivery: number;
     delivery_Local: string;
     material_type: string;
@@ -27,4 +25,78 @@ export class Delivery implements IDelivery{
     material_quantity: number;
     fk_user: User;
     fk_material: Material;
+
+    constructor(
+        id_delivery: number,
+        delivery_Local: string,
+        material_type: string,
+        status: status,
+        material_quantity: number,
+        fk_user: User,
+        fk_material: Material
+    ) {
+        this.id_delivery = id_delivery;
+        this.delivery_Local = delivery_Local;
+        this.material_type = material_type;
+        this.status = status;
+        this.material_quantity = material_quantity;
+        this.fk_user = fk_user;
+        this.fk_material = fk_material;
+    }
+
+    getId_delivery(): number {
+        return this.id_delivery;
+    }
+
+    setId_delivery(id_delivery: number): void {
+        this.id_delivery = id_delivery;
+    }
+
+    getDelivery_Local(): string {
+        return this.delivery_Local;
+    }
+
+    setDelivery_Local(delivery_Local: string): void {
+        this.delivery_Local = delivery_Local;
+    }
+
+    getMaterial_type(): string {
+        return this.material_type;
+    }
+
+    setMaterial_type(material_type: string): void {
+        this.material_type = material_type;
+    }
+
+    getStatus(): status {
+        return this.status;
+    }
+
+    setStatus(status: status): void {
+        this.status = status;
+    }
+
+    getMaterial_quantity(): number {
+        return this.material_quantity;
+    }
+
+    setMaterial_quantity(material_quantity: number): void {
+        this.material_quantity = material_quantity;
+    }
+
+    getFk_user(): User {
+        return this.fk_user;
+    }
+
+    setFk_user(fk_user: User): void {
+        this.fk_user = fk_user;
+    }
+
+    getFk_material(): Material {
+        return this.fk_material;
+    }
+
+    setFk_material(fk_material: Material): void {
+        this.fk_material = fk_material;
+    }
 }
